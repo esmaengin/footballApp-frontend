a177044d070acbfee683fb764e8cf824afa2c0bd
esmaengin <esmaengin91@gmail.com>
2025-02-21 00:54:20 +0300

    login page ui geliştirmeleri

--- src/components/pages/Login/Login.tsx
+++ src/components/pages/Login/Login.tsx
@@ -1,29 +1,82 @@
-import { Button, Input, Stack, Fieldset } from "@chakra-ui/react";
+import { Button, Input, Stack, Box, Text, Heading } from "@chakra-ui/react";
 import { Field } from "@/components/ui/field";
+import { LuArrowRight } from "react-icons/lu";
 
 export default function LoginPage() {
   return (
-    <Fieldset.Root>
-      <Stack>
-        <Fieldset.Legend>Giriş Yap</Fieldset.Legend>
-        <Fieldset.HelperText textStyle="lg">
-          Hesabınız yok mu? <Button variant="plain">Kayıt ol</Button>
-        </Fieldset.HelperText>
-      </Stack>
-
-      <Fieldset.Content>
-        <Field label="Name">
-          <Input name="name" />
-        </Field>
-
-        <Field label="Email address">
-          <Input name="email" type="email" />
-        </Field>
-      </Fieldset.Content>
-
-      <Button type="button" alignSelf="flex-start" colorPalette="red">
-        Giriş Yap
-      </Button>
-    </Fieldset.Root>
+    <Box
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      height="100vh"
+      backgroundColor="#d7e3d3"
+    >
+      <Box
+        maxWidth="400px"
+        width="100%"
+        py="18px"
+        px="40px"
+        border="2px solid #64905a"
+        borderRadius="4px"
+        background="#fff"
+        transition="all 0.3s ease"
+        justifyContent="center"
+      >
+        <Heading size="4xl" fontWeight="bold" textAlign="center">
+          Giriş Yap
+        </Heading>
+        <Stack direction="row" justify="center">
+          <Text mb="3" fontSize="md" color="fg.muted" textAlign="center">
+            Hesabınız yok mu?
+          </Text>
+          <Button
+            type="button"
+            variant="outline"
+            borderRadius="4px"
+            backgroundColor="#b0c7a9"
+            size="sm"
+          >
+            Hesap oluştur <LuArrowRight />
+          </Button>
+        </Stack>
+        <Stack justifyContent="center" alignItems="center" >
+          <Field label="Kullanıcı adı">
+            <br></br>
+            <Input
+              name="name"
+              marginBottom="12px"
+              borderRadius="4px"
+              height="24px"
+              width="full"
+              textAlign="center"
+            />
+          </Field>
+          <Field label="E-posta adresi">
+            <br></br>
+            <Input
+              name="email"
+              type="email"
+              height="24px"
+              width="full"
+              marginBottom="12px"
+              borderRadius="4px"
+            />
+          </Field>
+          <Button
+            type="button"
+            size="md"
+            backgroundColor="#008000"
+            variant="outline"
+            borderRadius="4px"
+            _hover={{
+              backgroundColor: "#2C7A7B",
+            }}
+          >
+            Giriş Yap
+          </Button>
+        </Stack>
+        
+      </Box>
+    </Box>
   );
 }
